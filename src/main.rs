a44d951b13c5bb833f7e6186614684c0dc3300e1
Adam Simpson <adam@heysparkbox.com>
2020-07-29 13:17:17 -0400

    feat: init commit

---
+++ src/main.rs
@@ -0,0 +1,35 @@
+use mailparse::*;
+use std::fs::File;
+use std::io::prelude::*;
+use std::io::Read;
+
+fn main() {
+    let file = std::env::args().nth(1).expect("mail message to parse");
+    let mut file_handle = File::open(file.trim()).expect("file handle to contents of message");
+    let mut contents = String::new();
+
+    file_handle
+        .read_to_string(&mut contents)
+        .expect("actual message content");
+    let mail = parse_mail(contents.as_bytes()).expect("parsed mail struct");
+    let message_id = mail
+        .headers
+        .get_first_value("Message-ID")
+        .expect("Message ID");
+    let file_name = format!("/tmp/{}", message_id);
+    let mut tmp_file = File::create(&file_name).expect("tmp file created");
+    let mut body = "".to_string();
+
+    if mail.subparts.len() > 0 {
+        for m in mail.subparts.iter() {
+            if m.ctype.mimetype == "text/plain" {
+                body = m.get_body().expect("message content");
+            }
+        }
+    } else {
+        body = mail.get_body().expect("message content");
+    }
+
+    println!("{}", &file_name);
+    tmp_file.write_all(body.as_bytes()).expect("file created");
+}
