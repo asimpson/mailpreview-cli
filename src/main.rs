356ba76f2e7e80b887d4416a651d357818e2614f
Adam Simpson <adam@heysparkbox.com>
2020-07-29 16:37:26 -0400

    refactor: make error handling and feedback better

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,11 @@
 use mailparse::*;
+use std::error::Error;
 use std::fs::File;
 use std::io::prelude::*;
 use std::io::Read;
 
 fn return_body(mail: ParsedMail) -> Result<String, MailParseError> {
-    let mut body = "".to_string();
+    let mut body = String::new();
 
     if mail.subparts.len() > 0 {
         for m in mail.subparts.iter() {
@@ -16,27 +17,33 @@ fn return_body(mail: ParsedMail) -> Result<String, MailParseError> {
         body = mail.get_body()?;
     }
 
-    return Ok(body);
+    Ok(body)
 }
 
-fn main() {
-    let file = std::env::args().nth(1).expect("mail message to parse");
-    let mut file_handle = File::open(file.trim()).expect("file handle to contents of message");
-    let mut contents = String::new();
+fn return_path_from_cli() -> Result<String, String> {
+    let file = std::env::args().nth(1);
 
-    file_handle
-        .read_to_string(&mut contents)
-        .expect("actual message content");
-    let mail = parse_mail(contents.as_bytes()).expect("parsed mail struct");
+    match file {
+        Some(f) => Ok(f),
+        None => Err("No message file was provided.".to_string()),
+    }
+}
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let file = return_path_from_cli()?;
+    let mut contents = String::new();
+    File::open(file.trim())?.read_to_string(&mut contents)?;
+
+    let mail = parse_mail(contents.as_bytes())?;
     let message_id = mail
         .headers
         .get_first_value("Message-ID")
         .expect("Message-ID");
     let file_name = format!("/tmp/{}", message_id);
-    let mut tmp_file = File::create(&file_name).expect("tmp file created");
     let body = return_body(mail).expect("parsed body");
 
-
+    File::create(&file_name)?.write_all(body.as_bytes())?;
     println!("{}", &file_name);
-    tmp_file.write_all(body.as_bytes()).expect("file created");
+
+    Ok(())
 }
